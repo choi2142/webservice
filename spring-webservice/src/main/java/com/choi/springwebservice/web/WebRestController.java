2b2fdbaa0bbc62185c9ecec32c669d83b70c67d4
choi <choi@DESKTOP-6CKVL6O>
2020-03-02 19:06:50 +0900

    second commit

--- spring-webservice/src/main/java/com/choi/springwebservice/web/WebRestController.java
+++ spring-webservice/src/main/java/com/choi/springwebservice/web/WebRestController.java
@@ -1,13 +1,29 @@
 package com.choi.springwebservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.choi.springwebservice.domain.posts.PostsRepostitory;
+import com.choi.springwebservice.dto.posts.PostsSaveRequestDto;
+
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+	
+	private PostsRepostitory postRepository;
 	
 	@GetMapping("/hello")
 	public String hello() {
 		return"HelloWorld";
+	}
+	
+	@PostMapping("/posts")
+	public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+		
+		postRepository.save(dto.toEntity());
 	}
 }
