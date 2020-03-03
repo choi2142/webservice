34fce5277e09f65968c8b256ef66084fd1843ae2
choi <choi@DESKTOP-6CKVL6O>
2020-03-03 20:50:55 +0900

    7

--- spring-webservice/src/test/java/com/choi/springwebservice/SpringWebserviceApplicationTests.java
+++ spring-webservice/src/test/java/com/choi/springwebservice/SpringWebserviceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.choi.springwebservice;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringWebserviceApplicationTests {
+public class SpringWebserviceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
